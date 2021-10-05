725dc32a53121fd51ffed037834e7f0af197f0af
sankardocu3c <91928952+sankardocu3c@users.noreply.github.com>
2021-10-05 22:10:02 +0530

    update index.js
    checking ci/cd

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const posts = require('./routes/api/posts')
 app.use('/api/posts', posts)
 
 
-// starting server
+// starting the server 
 const port = process.env.PORT || 5000;
 app.listen(port , () => console.log(`Server started on port : ${port}`));
 
